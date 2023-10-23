955182e897bf12432ba753e0e4536092cd6b4bf4
Graham Hughes <graehu@gmail.com>
2023-10-23 12:52:24 +0100

    fixing packaging

--- tdb/html.py
+++ tdb/html.py
@@ -2,7 +2,6 @@ import tdb.config
 try: import markdown
 except: pass
 import os
-import shutil
 
 _css_file = "/".join((tdb.config._tdb_dir, "style.css"))
 
@@ -43,6 +42,6 @@ def print_html(entries):
     entries_str = ""
     for in_entry in entries:
         try: in_entry["text"] = markdown.markdown(in_entry["text"], extensions=["extra", "codehilite"])
-        except: in_entry = "<pre>"+in_entry+"</pre>"
+        except: in_entry["text"] = "<pre>"+in_entry["text"]+"</pre>"
         entries_str += entry.format_map(in_entry)
     print(body.format_map({"css":_css, "css_file":_css_file, "entries":entries_str}))
