9e6f854c317bf802390ce102c896f174d0c078dc
Graham Hughes <graehu@gmail.com>
2023-11-13 22:20:19 +0000

    mermaid-md module support

--- tdb/html.py
+++ tdb/html.py
@@ -1,5 +1,8 @@
 import tdb.config
+import sys
 try: import markdown
+except: pass
+try: import md_mermaid
 except: pass
 import os
 
@@ -10,8 +13,8 @@ if not os.path.exists(_css_file): _css_file = "/".join((os.path.dirname(__file__
 _css = open(_css_file, "r").read()
 
 body = """<html>
-
       <header>
+         <script src="mermaid.min.js"></script>
         <style>
 /*inserted from {css_file}*/
 {css}
@@ -41,7 +44,12 @@ entry = """
 def print_html(entries, file=None):
     entries_str = ""
     for in_entry in entries:
-        try: in_entry["text"] = markdown.markdown(in_entry["text"], extensions=["extra", "codehilite"])
-        except: in_entry["text"] = "<pre>"+in_entry["text"]+"</pre>"
+        if "markdown" in sys.modules:
+            extensions = ["extra", "codehilite"]
+            if "md_mermaid" in sys.modules: extensions.append("md_mermaid")
+            try: in_entry["text"] = markdown.markdown(in_entry["text"], extensions=extensions)
+            except: in_entry["text"] = markdown.markdown(in_entry["text"], extensions=["extra", "codehilite"])
+        else:
+            in_entry["text"] = "<pre>"+in_entry["text"]+"</pre>"
         entries_str += entry.format_map(in_entry)
     print(body.format_map({"css":_css, "css_file":_css_file, "entries":entries_str}), file=file)
